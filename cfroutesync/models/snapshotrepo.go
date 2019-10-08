d9ff48bbaeefac4b997c14316f5fb06a0723a6e6
Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
2019-10-08 11:15:25 -0700

    snapshotrepo is safe for concurrent use
    [#168270861]
    
    Co-authored-by: Gabe Rosenhouse <grosenhouse@pivotal.io>

--- cfroutesync/models/snapshotrepo.go
+++ cfroutesync/models/snapshotrepo.go
@@ -1,6 +1,9 @@
 package models
 
+import "sync"
+
 type SnapshotRepo struct {
+	mutex    sync.RWMutex
 	snapshot *RouteSnapshot
 }
 
@@ -28,11 +31,16 @@ type DestinationApp struct {
 }
 
 func (r *SnapshotRepo) Get() (*RouteSnapshot, bool) {
+	r.mutex.RLock()
+	defer r.mutex.RUnlock()
+	if r.snapshot == nil {
+		return nil, false
+	}
 	return r.snapshot, true
 }
 
 func (r *SnapshotRepo) Put(snapshot *RouteSnapshot) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
 	r.snapshot = snapshot
 }
-
-// TODO: Expect: it is safe to call Get and Replace at the same time
---
+++ cfroutesync/models/snapshotrepo_test.go
@@ -0,0 +1,72 @@
+package models_test
+
+import (
+	"sync"
+
+	"code.cloudfoundry.org/cf-k8s-networking/cfroutesync/models"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("SnapshotRepo", func() {
+	Specify("Get returns the snapshot that was Put in", func() {
+		repo := models.SnapshotRepo{}
+		thing := &models.RouteSnapshot{
+			Routes: []*models.Route{
+				{Guid: "foo"},
+				{Guid: "bar"},
+			},
+		}
+		repo.Put(thing)
+
+		snapshot, ok := repo.Get()
+
+		Expect(ok).To(BeTrue())
+		Expect(snapshot).To(Equal(thing))
+	})
+
+	Context("when no snapshot has been Put into the repo", func() {
+		Specify("Get returns nil,false", func() {
+			repo := models.SnapshotRepo{}
+
+			snapshot, ok := repo.Get()
+			Expect(snapshot).To(BeNil())
+
+			Expect(ok).To(BeFalse())
+		})
+	})
+
+	// this test is only meaningful if run using the -race flag
+	Specify("the repo is safe for concurrent access", func() {
+		repo := &models.SnapshotRepo{}
+		const numCalls = 100
+
+		var complete sync.WaitGroup
+		complete.Add(2)
+
+		go func(repo *models.SnapshotRepo) {
+			for i := 0; i < numCalls; i++ {
+				thing := &models.RouteSnapshot{
+					Routes: []*models.Route{
+						{Guid: "foo"},
+						{Guid: "bar"},
+					},
+				}
+				repo.Put(thing)
+			}
+			complete.Done()
+		}(repo)
+
+		go func(repo *models.SnapshotRepo) {
+			for i := 0; i < numCalls; i++ {
+				repo.Get()
+			}
+			complete.Done()
+		}(repo)
+
+		complete.Wait()
+		// if we've made it this far without a race detected, then
+		// the snapshotRepo is safe for concurrent use!
+	})
+})
