6331b226d2dcae0ba0a030a8effae60f6d8feff2
Rodolfo Sanchez <rsanchez@pivotal.io>
2019-10-25 14:17:23 -0700

    ci: add task for deploying Istio and configuring DNS
    [#169129559](https://www.pivotaltracker.com/story/show/169129559)
    
    Co-authored-by: Tim Downey <tdowney@pivotal.io>

---
+++ ci/tasks/istio/deploy-istio.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function main() {
+  service_key_path="${tmp_dir}/gcp.json"
+  echo "${GCP_DNS_SERVICE_ACCOUNT_KEY}" > "${service_key_path}"
+  gcloud auth activate-service-account --key-file="${service_key_path}"
+  gcloud config set project "${GCP_PROJECT_ID}"
+
+  workspace=${PWD}
+  export KUBECONFIG="${PWD}/kubeconfig/config"
+
+  pushd istio > /dev/null
+    kubectl config use-context ${KUBECONFIG_CONTEXT}
+    kubectl create namespace istio-system || true
+
+    # Install Istio CRDs
+    helm template install/kubernetes/helm/istio-init --name istio-init --namespace istio-system | kubectl apply -f -
+
+    # Install Istio
+    helm template install/kubernetes/helm/istio --name istio --namespace istio-system | kubectl apply -f -
+
+    # Create default ingress gateway for Cloud Foundry
+    kubectl create namespace cf-workloads || true
+    kubectl apply -f "${workspace}/cf-k8s-networking/ci/istio-config/cf-workloads-gateway.yml"
+
+    echo "Discovering Istio Gateway LB IP"
+    external_static_ip=""
+    while [ -z $external_static_ip ]; do
+        sleep 10
+        external_static_ip=$(kubectl get services/istio-ingressgateway -n istio-system --output="jsonpath={.status.loadBalancer.ingress[0].ip}")
+    done
+
+    echo "Configuring DNS for external IP: ${external_static_ip}"
+    gcloud dns record-sets transaction start --zone="${SHARED_DNS_ZONE_NAME}"
+    gcp_records_json="$( gcloud dns record-sets list --zone "${SHARED_DNS_ZONE_NAME}" --name "*.${DNS_DOMAIN}" --format=json )"
+    record_count="$( echo "${gcp_records_json}" | jq 'length' )"
+    if [ "${record_count}" != "0" ]; then
+      existing_record_ip="$( echo "${gcp_records_json}" | jq -r '.[0].rrdatas | join(" ")' )"
+      gcloud dns record-sets transaction remove --name "*.${DNS_DOMAIN}" --type=A --zone="${SHARED_DNS_ZONE_NAME}" --ttl=300 "${existing_record_ip}" --verbosity=debug
+    fi
+    gcloud dns record-sets transaction add --name "*.${DNS_DOMAIN}" --type=A --zone="${SHARED_DNS_ZONE_NAME}" --ttl=300 "${external_static_ip}" --verbosity=debug
+
+    echo "Contents of transaction.yaml:"
+    cat transaction.yaml
+    gcloud dns record-sets transaction execute --zone="${SHARED_DNS_ZONE_NAME}" --verbosity=debug
+
+  popd
+}
+
+main
