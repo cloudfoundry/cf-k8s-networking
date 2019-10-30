18137ccf4b6844491c598906c70cc5727cee7cf7
Tim Downey <tdowney@pivotal.io>
2019-10-30 15:19:36 -0700

    ci: install metacontroller on eirini-dev-1
    - introduces generic kubectl-apply task that can apply an arbitrary
    ordered list of k8s configuration files
    
    [#168852248](https://www.pivotaltracker.com/story/show/168852248)
    
    Co-authored-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

---
+++ ci/tasks/k8s/kubectl-apply.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# ENV
+: "${KUBECONFIG_CONTEXT:?}"
+: "${FILES_TO_APPLY:?}"
+
+function kubectl_apply_all() {
+  workspace=${PWD}
+  export KUBECONFIG="${workspace}/kubeconfig/config"
+
+  pushd k8s-config-dir > /dev/null
+    kubectl config use-context ${KUBECONFIG_CONTEXT}
+
+    for file in ${FILES_TO_APPLY}
+    do
+        echo "Applying ${file}"
+        kubectl apply -f
+        sleep 5  # give k8s time to converge
+    done
+  popd
+}
+
+function main() {
+  kubectl_apply_all
+}
+
+main
