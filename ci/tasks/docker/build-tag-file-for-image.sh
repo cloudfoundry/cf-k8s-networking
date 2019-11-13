86dedd36677c6ce59f2d54da36d74e76bebd1a5e
Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
2019-11-13 14:14:57 -0800

    ci: build image and run unit tests in pipeline
    - this will allow us to align publishing an image and deploying that
    image to our environments based on a specific commit
    
    [#169629058](https://www.pivotaltracker.com/story/show/169629058)
    
    Co-authored-by: Rodolfo Sanchez <rsanchez@pivotal.io>
    Co-authored-by: Tim Downey <tdowney@pivotal.io>

---
+++ ci/tasks/docker/build-tag-file-for-image.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+pushd cf-k8s-networking > /dev/null
+    git_sha="$(cat .git/ref)"
+    branch_name="$(ls .git/refs/heads)"
+popd
+
+echo "${branch_name} ${git_sha}" > docker-info/tags
