e03f895f14c7dfc8657755beec801d624f85c75d
Rodolfo Sanchez <rsanchez@pivotal.io>
2019-11-14 11:17:09 -0800

    ci: create second dev environment
    - enable Istio automatic sidecar injection in this environment
    
    [#169324830](https://www.pivotaltracker.com/story/show/169324830)
    
    Co-authored-by: Tim Downey <tdowney@pivotal.io>

---
+++ ci/tasks/istio/enable-sidecar-injection.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# ENV
+: "${KUBECONFIG_CONTEXT:?}"
+
+function enabled_sidecar_injection() {
+  workspace=${PWD}
+  export KUBECONFIG="${PWD}/kubeconfig/config"
+
+  # Enable Istio Sidecar Injection for app workloads
+  kubectl label namespace cf-workloads istio-injection=enabled --overwrite=true
+}
+
+function main() {
+  enabled_sidecar_injection
+}
+
+main
