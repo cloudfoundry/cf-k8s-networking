ecc842148db2510271e45e781550ef989500f32d
Tim Downey <tdowney@pivotal.io>
2019-11-26 16:09:16 -0800

    ci: run a minimal subset of CATS instead of cf-smoke-tests
    - our routing tier doesn't support TLS termination at the ingress router
    yet which is currently required by cf-smoke-tests
    - once we do https://www.pivotaltracker.com/story/show/169691238 and
    have app logs working again we could switch back to using them
    
    [#169909387](https://www.pivotaltracker.com/story/show/169909387)
    
    Co-authored-by: Rodolfo Sanchez <rsanchez@pivotal.io>

--- ci/tasks/bbl/seed-env.sh
+++ ci/tasks/bbl/seed-env.sh
@@ -53,6 +53,51 @@ EOF
   fi
 }
 
+smoke_config_output_path="smoke_cats_integration_config.json"
+
+function write_smoke_test_config() {
+  if [ ! -f "${smoke_config_output_path}" ]; then
+    cat <<- EOF > "${smoke_config_output_path}"
+{
+    "api": "api.${SYSTEM_DOMAIN}",
+    "apps_domain": "${APPS_DOMAIN}",
+    "admin_user": "admin",
+    "admin_password": "will-be-overridden-by-later-task",
+    "skip_ssl_validation": true,
+    "use_http": true,
+    "backend": "diego",
+    "default_timeout": 60,
+    "include_apps": false,
+    "include_v3": false,
+    "include_capi_experimental": false,
+    "include_capi_no_bridge": false,
+    "include_routing": true,
+    "include_detect": false,
+    "include_sso": false,
+    "include_container_networking": false,
+    "include_backend_compatability": false,
+    "include_credhub": false,
+    "include_deployments": false,
+    "include_docker": false,
+    "include_internet_dependent": false,
+    "include_isolation_segments": false,
+    "include_private_docker_registry": false,
+    "include_route_services": false,
+    "include_routing_isolation_segments": false,
+    "include_security_groups": false,
+    "include_service_discovery": false,
+    "include_services": false,
+    "include_service_instance_sharing": false,
+    "include_tasks": false,
+    "include_ssh": false,
+    "include_zipkin": false,
+    "use_existing_user": false,
+    "keep_user_at_suite_end": false
+}
+EOF
+  fi
+}
+
 apps_domain_vars_file="apps_domains_vars.yml"
 function write_apps_domains_vars_file() {
   if [ ! -f "${apps_domain_vars_file}" ]; then
@@ -98,6 +143,7 @@ output_path="updated-bbl-state/${BBL_STATE_DIR}"
 mkdir -p "${output_path}"
 pushd ${output_path}
     write_cats_config
+    write_smoke_test_config
     write_apps_domains_vars_file
     write_load_balancer_certs
 
