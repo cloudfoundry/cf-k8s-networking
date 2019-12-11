2e3582e6c8f644b926d01c7d58a6fe7d28bf7ee8
Mikael Manukyan <mmanukyan@pivotal.io>
2019-12-11 10:59:00 -0800

    ci: fix install-grafana-dashboard job cleanup

--- ci/tasks/istio/install-grafana-dashboard.sh
+++ ci/tasks/istio/install-grafana-dashboard.sh
@@ -10,7 +10,7 @@ function install_grafana_dashboard() {
   kubectl config use-context ${KUBECONFIG_CONTEXT}
 
   dashboard_file="${PWD}/cf-k8s-networking/doc/metrics/dashboard.json"
-  kubectl delete jobs create-dashboard
+  kubectl delete jobs create-dashboard  --ignore-not-found=true
   kubectl run --generator=job/v1 create-dashboard --image alpine --restart Never --dry-run -o json --command -- \
     sh -c "apk add curl && curl -H 'Content-Type: application/json' -H 'Accept: application/json' -XPOST http://grafana.istio-system.svc.cluster.local:3000/api/dashboards/db -d '""$(jq -n '{ "dashboard": input }'  $dashboard_file | jq '.dashboard.id = null' | jq '.dashboard.uid = null' -c)""'" | \
     jq '.spec.ttlSecondsAfterFinished = 10' | \
