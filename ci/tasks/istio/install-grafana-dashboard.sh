815af5be7bd3fb522a4560b78610a9e2ae30693a
Mikael Manukyan <mmanukyan@pivotal.io>
2020-01-09 15:35:20 -0800

    ci: fix grafana install task
    Co-authored-by: Rodolfo Sanchez <rsanchez@pivotal.io>

--- ci/tasks/istio/install-grafana-dashboard.sh
+++ ci/tasks/istio/install-grafana-dashboard.sh
@@ -10,11 +10,11 @@ function install_grafana_dashboard() {
   kubectl config use-context ${KUBECONFIG_CONTEXT}
 
   dashboard_file="${PWD}/cf-k8s-networking/doc/metrics/dashboard.json"
-  kubectl delete jobs create-dashboard  --ignore-not-found=true
-  kubectl run --generator=job/v1 create-dashboard --image alpine --restart Never --dry-run -o json --command -- \
-    sh -c "apk add curl && curl -H 'Content-Type: application/json' -H 'Accept: application/json' -XPOST http://grafana.istio-system.svc.cluster.local:3000/api/dashboards/db -d '""$(jq -n '{ "dashboard": input }'  $dashboard_file | jq '.dashboard.id = null' | jq '.dashboard.uid = null' -c)""'" | \
-    jq '.spec.ttlSecondsAfterFinished = 10' | \
-    kubectl apply -f -
+
+  jq -n '{ "dashboard": input }'  $dashboard_file | jq '.dashboard.id = null' | jq '.dashboard.uid = null' -c > "/tmp/dashboard.json"
+
+  kubectl proxy --port=8080
+  curl -H 'Content-Type: application/json' -H 'Accept: application/json' -XPOST http://localhost:8080/api/v1/namespaces/istio-system/services/grafana:http/proxy/api/dashboards/db -d "@/tmp/dashboard.json"
 }
 
 
