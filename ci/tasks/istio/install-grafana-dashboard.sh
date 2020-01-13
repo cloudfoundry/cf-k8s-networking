c724d2fa0d9bd5998f593a3c9e5f17e372c68b9c
Mikael Manukyan <mmanukyan@pivotal.io>
2020-01-13 14:33:21 -0800

    ci: delete old grafana dashboard before creating new one
    Co-authored-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- ci/tasks/istio/install-grafana-dashboard.sh
+++ ci/tasks/istio/install-grafana-dashboard.sh
@@ -11,10 +11,17 @@ function install_grafana_dashboard() {
 
   dashboard_file="${PWD}/cf-k8s-networking/doc/metrics/dashboard.json"
 
-  jq -n '{ "dashboard": input }'  $dashboard_file | jq '.dashboard.id = null' | jq '.dashboard.uid = null' -c > "/tmp/dashboard.json"
+  jq -n '{ "dashboard": input }'  $dashboard_file | jq '.dashboard.id = null' | jq '.dashboard.uid = "indicators"' -c > "/tmp/dashboard.json"
 
-  kubectl proxy --port=8080
+  kubectl proxy --port=8080 &
+  proxy_pid=$!
+
+  # Delete old dashboard
+  curl -H 'Content-Type: application/json' -H 'Accept: application/json' -XDELETE http://localhost:8080/api/v1/namespaces/istio-system/services/grafana:http/proxy/api/dashboards/uid/indicators
+  # Create dashboard
   curl -H 'Content-Type: application/json' -H 'Accept: application/json' -XPOST http://localhost:8080/api/v1/namespaces/istio-system/services/grafana:http/proxy/api/dashboards/db -d "@/tmp/dashboard.json"
+
+  kill ${proxy_pid}
 }
 
 
