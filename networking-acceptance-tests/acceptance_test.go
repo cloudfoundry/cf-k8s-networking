8116e9d8bf623757b93819dea7a56935219ae28b
Mikael Manukyan <mmanukyan@pivotal.io>
2020-02-05 10:45:17 -0800

    test: move config to separate package for acceptance tests
    [#170899741](https://www.pivotaltracker.com/story/show/170899741)

--- networking-acceptance-tests/acceptance_test.go
+++ networking-acceptance-tests/acceptance_test.go
@@ -1,13 +1,12 @@
 package acceptance_test
 
 import (
-	"encoding/json"
+	config2 "code.cloudfoundry.org/cf-k8s-networking/acceptance/config"
 	"fmt"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/cf"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/generator"
 	"github.com/cloudfoundry-incubator/cf-test-helpers/workflowhelpers"
 	"github.com/onsi/gomega/gexec"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -29,7 +28,7 @@ var (
 func TestAcceptance(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	config, err := NewConfig(os.Getenv("CONFIG"))
+	config, err := config2.NewConfig(os.Getenv("CONFIG"))
 	if err != nil {
 		defer GinkgoRecover()
 		fmt.Println("Failed to load config.")
@@ -65,114 +64,6 @@ func TestAcceptance(t *testing.T) {
 	RunSpecs(t, "Acceptance Suite")
 }
 
-type Config struct {
-	KubeConfigPath string `json:"kubeconfig_path"`
-
-	API            string `json:"api"`
-	AdminUser      string `json:"admin_user"`
-	AdminPassword  string `json:"admin_password"`
-
-	ExistingUser         string `json:"existing_user"`
-	ExistingUserPassword string `json:"existing_user_password"`
-	ShouldKeepUser       bool   `json:"keep_user_at_suite_end"`
-	UseExistingUser      bool   `json:"use_existing_user"`
-
-	UseExistingOrganization bool   `json:"use_existing_organization"`
-	ExistingOrganization    string `json:"existing_organization"`
-}
-
-func (c *Config) GetAdminUser() string {
-	return c.AdminUser
-}
-
-func (c *Config) GetAdminPassword() string {
-	return c.AdminPassword
-}
-
-func (c *Config) GetUseExistingOrganization() bool {
-	return c.UseExistingOrganization
-}
-
-func (c *Config) GetUseExistingSpace() bool {
-	return false
-}
-
-func (c *Config) GetExistingOrganization() string {
-	return c.ExistingOrganization
-}
-
-func (c *Config) GetExistingSpace() string {
-	panic("implement me")
-}
-
-func (c *Config) GetUseExistingUser() bool {
-	return c.UseExistingUser
-}
-
-func (c *Config) GetExistingUser() string {
-	return c.ExistingUser
-}
-
-func (c *Config) GetExistingUserPassword() string {
-	return c.ExistingUserPassword
-}
-
-func (c *Config) GetShouldKeepUser() bool {
-	return c.ShouldKeepUser
-}
-
-func (c *Config) GetConfigurableTestPassword() string {
-	return ""
-}
-
-func (c *Config) GetAdminClient() string {
-	return ""
-}
-
-func (c *Config) GetAdminClientSecret() string {
-	return ""
-}
-
-func (c *Config) GetExistingClient() string {
-	return ""
-}
-
-func (c *Config) GetExistingClientSecret() string {
-	panic("implement me")
-}
-
-func (c *Config) GetApiEndpoint() string {
-	return c.API
-}
-
-func (c *Config) GetSkipSSLValidation() bool {
-	return true
-}
-
-func (c *Config) GetNamePrefix() string {
-	return "ACCEPTANCE"
-}
-
-func (c *Config) GetScaledTimeout(timeout time.Duration) time.Duration {
-	return time.Duration(float64(timeout) * 2)
-}
-
-func NewConfig(configPath string) (*Config, error) {
-	configFile, err := ioutil.ReadFile(configPath)
-	if err != nil {
-		return nil, fmt.Errorf("error reading config %v", err)
-	}
-
-	config := &Config{}
-	err = json.Unmarshal([]byte(configFile), config)
-
-	if err != nil {
-		return nil, fmt.Errorf("error parsing json %v", err)
-	}
-
-	return config, nil
-}
-
 type KubeCtl struct {
 	kubeConfigPath string
 }
@@ -182,7 +73,7 @@ func (kc *KubeCtl) Run(args ...string) ([]byte, error) {
 	cmd.Env = []string{
 		fmt.Sprintf("KUBECONFIG=%s", kc.kubeConfigPath),
 		fmt.Sprintf("PATH=%s", os.Getenv("PATH")),
-		fmt.Sprintf("HOME=%s", filepath.Dir(kc.kubeConfigPath)), // fixme
+		fmt.Sprintf("HOME=%s", filepath.Dir(kc.kubeConfigPath)), // fixme because kubectl will create another .kube folder inside our provided kubeConfgPath
 	}
 	fmt.Fprintf(GinkgoWriter, "+ Run %s\n", strings.Join(args, " "))
 	output, err := cmd.CombinedOutput()
---
+++ networking-acceptance-tests/config/config.go
@@ -0,0 +1,124 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"time"
+)
+
+// Example config:
+// 	{
+// 	  "kubeconfig_path": "/Users/user/.kube/config",
+// 	  "api": "api.example.com",
+// 	  "admin_user": "admin",
+// 	  "admin_password": "PASSWORD"
+// 	}
+type Config struct {
+	KubeConfigPath string `json:"kubeconfig_path"`
+
+	API           string `json:"api"`
+	AdminUser     string `json:"admin_user"`
+	AdminPassword string `json:"admin_password"`
+
+	ExistingUser         string `json:"existing_user"`
+	ExistingUserPassword string `json:"existing_user_password"`
+	ShouldKeepUser       bool   `json:"keep_user_at_suite_end"`
+	UseExistingUser      bool   `json:"use_existing_user"`
+
+	UseExistingOrganization bool   `json:"use_existing_organization"`
+	ExistingOrganization    string `json:"existing_organization"`
+}
+
+func (c *Config) GetAdminUser() string {
+	return c.AdminUser
+}
+
+func (c *Config) GetAdminPassword() string {
+	return c.AdminPassword
+}
+
+func (c *Config) GetUseExistingOrganization() bool {
+	return c.UseExistingOrganization
+}
+
+func (c *Config) GetUseExistingSpace() bool {
+	return false
+}
+
+func (c *Config) GetExistingOrganization() string {
+	return c.ExistingOrganization
+}
+
+func (c *Config) GetExistingSpace() string {
+	panic("implement me")
+}
+
+func (c *Config) GetUseExistingUser() bool {
+	return c.UseExistingUser
+}
+
+func (c *Config) GetExistingUser() string {
+	return c.ExistingUser
+}
+
+func (c *Config) GetExistingUserPassword() string {
+	return c.ExistingUserPassword
+}
+
+func (c *Config) GetShouldKeepUser() bool {
+	return c.ShouldKeepUser
+}
+
+func (c *Config) GetConfigurableTestPassword() string {
+	return ""
+}
+
+func (c *Config) GetAdminClient() string {
+	return ""
+}
+
+func (c *Config) GetAdminClientSecret() string {
+	return ""
+}
+
+func (c *Config) GetExistingClient() string {
+	return ""
+}
+
+func (c *Config) GetExistingClientSecret() string {
+	panic("implement me")
+}
+
+func (c *Config) GetApiEndpoint() string {
+	return c.API
+}
+
+func (c *Config) GetSkipSSLValidation() bool {
+	return true
+}
+
+func (c *Config) GetNamePrefix() string {
+	return "ACCEPTANCE"
+}
+
+func (c *Config) GetScaledTimeout(timeout time.Duration) time.Duration {
+	return time.Duration(float64(timeout) * 2)
+}
+
+func NewConfig(configPath string) (*Config, error) {
+	configFile, err := ioutil.ReadFile(configPath)
+	if err != nil {
+		return nil, fmt.Errorf("error reading config %v", err)
+	}
+
+	config := &Config{}
+	err = json.Unmarshal([]byte(configFile), config)
+
+	if err != nil {
+		return nil, fmt.Errorf("error parsing json %v", err)
+	}
+
+	return config, nil
+}
+
