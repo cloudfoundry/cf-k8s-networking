328f91c65a4cef4fa0f3da4f35b1b81c1f9eedf3
Rodolfo Sanchez <rsanchez@pivotal.io>
2020-02-04 16:50:38 -0800

    fix: script space thinks it is a function and not va
    Co-authored-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- ci/tasks/k8s/extract-kubeconfig.sh
+++ ci/tasks/k8s/extract-kubeconfig.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 
 function main() {
   mkdir -p $PWD/kubeconfig
-  kubeconfig_path = "${PWD}/kubeconfig/config"
-  bbl_state = "bbl-state/${BBL_STATE_DIR}"
+  kubeconfig_path="${PWD}/kubeconfig/config"
+  bbl_state="bbl-state/${BBL_STATE_DIR}"
 
   source "cf-k8s-networking-ci/tasks/k8s/utils.sh"
   extract_kubeconfig $kubeconfig_path $bbl_state
