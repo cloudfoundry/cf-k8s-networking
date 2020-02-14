ad234e1fb24eebf6ef5cb387d365668e6852d8fa
Tim Downey <tdowney@pivotal.io>
2020-02-14 10:13:19 -0800

    ci: add custom gke networking acceptance tests
    [#171165100](https://www.pivotaltracker.com/story/show/171165100)
    
    Co-authored-by: Rodolfo Sanchez <rsanchez@pivotal.io>

---
+++ ci/tasks/tests/run-networking-acceptance-gke.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -euo pipefail
+
+# ENV
+: "${INTEGRATION_CONFIG_FILE:?}"
+: "${CLUSTER_NAME:?}"
+: "${CLOUDSDK_COMPUTE_REGION:?}"
+: "${CLOUDSDK_COMPUTE_ZONE:?}"
+: "${GCP_SERVICE_ACCOUNT_KEY:?}"
+: "${GCP_PROJECT:?}"
+
+function extract_kubeconfig() {
+    export KUBECONFIG=kubeconfig/config
+
+    gcloud auth activate-service-account --key-file=<(echo "${GCP_SERVICE_ACCOUNT_KEY}") --project="${GCP_PROJECT}" 1>/dev/null 2>&1
+    gcloud container clusters get-credentials ${CLUSTER_NAME} 1>/dev/null 2>&1
+
+    local kubeconfig_path="${PWD}/${KUBECONFIG}"
+    local config="${PWD}/integration-config/${INTEGRATION_CONFIG_FILE}"
+
+    pushd cf-k8s-networking/networking-acceptance-tests > /dev/null
+        ./bin/test_local "${config}" "${kubeconfig_path}"
+    popd
+}
+
+main
