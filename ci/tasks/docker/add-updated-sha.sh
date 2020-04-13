fec2eb322d9a7f93096acf17f074353d5aae33cd
Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
2020-04-13 10:52:57 -0700

    ci: task to pin cfroutesync image digest when there are changes to cfroutesync
    [#172087151](https://www.pivotaltracker.com/story/show/172087151)
    
    Co-authored-by: Alex Standke <astandke@pivotal.io>

---
+++ ci/tasks/docker/add-updated-sha.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+pushd k8s-deploy-image > /dev/null
+    digest="$(cat digest)"
+popd
+
+pushd cf-k8s-networking
+    sed -i '' "s/cf-k8s-networking\/cfroutesync:.*/cf-k8s-networking\/cfroutesync:sha256@$digest/" config/cfroutesync/values.yaml
+
+    git config user.name "${GIT_COMMIT_USERNAME}"
+    git config user.email "${GIT_COMMIT_EMAIL}"
+
+    if [[ -n $(git status --porcelain) ]]; then
+        echo "changes detected, will commit..."
+        git add config/cfroutesync/values.yaml
+        git commit -m "Update cfroutesync image digest to ${digest}"
+
+        git log -1 --color | cat
+    else
+        echo "no changes in repo, no commit necessary"
+    fi
+popd
