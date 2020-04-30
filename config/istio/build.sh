0af33a39d7d9951b3bae88823aacd0ea1dfde354
Dave Walter <dwalter@pivotal.io>
2020-04-30 16:44:10 -0700

    Update istio build script to use kbld
    ... to pin the istio images to digests
    
    Co-authored-by: Andrew Costa <acosta@pivotal.io>

--- config/istio/build.sh
+++ config/istio/build.sh
@@ -3,4 +3,4 @@ set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-${SCRIPT_DIR}/generate.sh "$@"  > "${SCRIPT_DIR}/../istio-generated/xxx-generated-istio.yaml"
+${SCRIPT_DIR}/generate.sh "$@" | kbld -f - > "${SCRIPT_DIR}/../istio-generated/xxx-generated-istio.yaml"
