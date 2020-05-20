24b63133ca226f35c0aaec96d5357e4141b4e844
Jen Spinney <jspinney@pivotal.io>
2020-05-20 17:37:09 +0000

    fix: typo in script flag [#172922921](https://www.pivotaltracker.com/story/show/172922921)
    Co-authored-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- ci/tasks/docker/generate-image-labels.sh
+++ ci/tasks/docker/generate-image-labels.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 deplab \
   --image-tar initial-image/rootfs.tar \
   --git cf-k8s-networking \
-  --metadata metadata.json
+  --metadata-file metadata.json
 
 echo -n '{"io.pivotal.metadata": ' > labels/labels.json
 cat metadata.json | jq '. | tostring' >> labels/labels.json
