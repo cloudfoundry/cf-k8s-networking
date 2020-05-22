44fcd4c7050bf704053f20cae28b041d64d6fcc8
Christian Ang <angc@vmware.com>
2020-05-22 21:00:48 +0000

    Update smoke test apps domain with apps prefix
    - the apps domain has been changed to use apps so we can make requests
    over https

--- ci/tasks/cf4k8s/run-smoke-test.sh
+++ ci/tasks/cf4k8s/run-smoke-test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 function run_smoke_test() {
     DNS_DOMAIN=$(cat env-metadata/dns-domain.txt)
     export SMOKE_TEST_API_ENDPOINT="https://api.${DNS_DOMAIN}"
-    export SMOKE_TEST_APPS_DOMAIN="${DNS_DOMAIN}"
+    export SMOKE_TEST_APPS_DOMAIN="apps.${DNS_DOMAIN}"
     export SMOKE_TEST_USERNAME=admin
     export SMOKE_TEST_PASSWORD=$(cat env-metadata/cf-admin-password.txt)
     cf-for-k8s-master/hack/run-smoke-tests.sh
