e1d65dc4581b97733b892f8bae2b9686242a3180
Alex Standke <astandke@vmware.com>
2020-06-02 23:46:44 +0000

    ci: make changes _before_ committing
    Co-authored-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- ci/tasks/tests/stress/run-stress-tests.sh
+++ ci/tasks/tests/stress/run-stress-tests.sh
@@ -8,6 +8,7 @@ cp routecontroller-stress-results/results.json cf-k8s-networking/routecontroller
 
 concourse-dcind/entrypoint.sh cf-k8s-networking/routecontroller/scripts/stress
 
+cp cf-k8s-networking/routecontroller/stress/results.json routecontroller-stress-results/results.json
 pushd routecontroller-stress-results
     git config user.name "${GIT_COMMIT_USERNAME}"
     git config user.email "${GIT_COMMIT_EMAIL}"
@@ -16,6 +17,5 @@ pushd routecontroller-stress-results
 popd
 
 shopt -s dotglob
-cp cf-k8s-networking/routecontroller/stress/ routecontroller-stress-results/results.json
 cp -r routecontroller-stress-results/* routecontroller-stress-results-modified
 
