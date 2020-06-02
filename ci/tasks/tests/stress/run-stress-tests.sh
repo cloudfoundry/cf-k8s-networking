4bdd152ffef59876a39d85b74a462271ccc0504d
Alex Standke <astandke@vmware.com>
2020-06-02 23:31:30 +0000

    ci: make stress commits as cf-networking-bot
    Co-authored-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- ci/tasks/tests/stress/run-stress-tests.sh
+++ ci/tasks/tests/stress/run-stress-tests.sh
@@ -8,6 +8,9 @@ cp routecontroller-stress-results/results.json cf-k8s-networking/routecontroller
 
 concourse-dcind/entrypoint.sh cf-k8s-networking/routecontroller/scripts/stress
 
+git config user.name "${GIT_COMMIT_USERNAME}"
+git config user.email "${GIT_COMMIT_EMAIL}"
+
 shopt -s dotglob
 pushd routecontroller-stress-results
     git add .
