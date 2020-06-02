62239657f0cee6d8320600f608d6d4e9b7981073
Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
2020-06-02 21:27:23 +0000

    ci: extract stress test runner into task/script
    [#172969192](https://www.pivotaltracker.com/story/show/172969192)
    
    Co-authored-by: Alex Standke <astandke@vmware.com>

---
+++ ci/tasks/tests/stress/run-stress-tests.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+concourse-dcind/entrypoint.sh cf-k8s-networking/routecontroller/scripts/stress
+
+shopt -s dotglob
+pushd routecontroller-stress-results
+    git add .
+    git commit -m "Stress test results"
+popd
+
+cp -r routecontroller-stress-results/* routecontroller-stress-results-modified
+
