c140e2fbe98910fb12a619d8a75db8975f90ecac
Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
2020-06-03 18:59:28 +0000

    ci: stress results can be tied to a commit SHA of cf-k8s-networking
    [#172969192](https://www.pivotaltracker.com/story/show/172969192)
    
    Co-authored-by: Tim Downey <tdowney@vmware.com>

--- ci/tasks/tests/stress/run-stress-tests.sh
+++ ci/tasks/tests/stress/run-stress-tests.sh
@@ -9,11 +9,16 @@ cp routecontroller-stress-results/results.json cf-k8s-networking/routecontroller
 concourse-dcind/entrypoint.sh cf-k8s-networking/routecontroller/scripts/stress
 
 cp cf-k8s-networking/routecontroller/stress/results.json routecontroller-stress-results/results.json
+
+pushd cf-k8s-networking > /dev/null
+    git_sha="$(cat .git/ref)"
+popd
+
 pushd routecontroller-stress-results
     git config user.name "${GIT_COMMIT_USERNAME}"
     git config user.email "${GIT_COMMIT_EMAIL}"
     git add .
-    git commit -m "Stress test results"
+    git commit -m "Stress test results for cf-k8s-networking commit SHA ${git_sha}"
 popd
 
 shopt -s dotglob
