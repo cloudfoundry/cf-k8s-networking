c16b604234fd2002a629ed886bfa85a464e5fd76
Alex Standke <astandke@vmware.com>
2020-07-07 22:21:18 +0000

    ci: increase time limit for routecontroller stress tests
    - intial values based on dev machines
    
    [#173618082](https://www.pivotaltracker.com/story/show/173618082)
    
    Co-authored-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>

--- routecontroller/stress/stress_test.go
+++ routecontroller/stress/stress_test.go
@@ -52,11 +52,11 @@ var _ = Describe("Stress Tests", func() {
 		}
 
 		for _, res := range results.Add100P95 {
-			Expect(res).To(BeNumerically("<", 0.25))
+			Expect(res).To(BeNumerically("<", 0.5))
 		}
 
 		for _, res := range results.Del100P95 {
-			Expect(res).To(BeNumerically("<", 0.25))
+			Expect(res).To(BeNumerically("<", 0.5))
 		}
 
 		for _, res := range results.Del1000P95 {
