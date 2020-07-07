dd508b2b218e2df7a66f6b7bb29af287eb63e5bc
Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
2020-07-07 21:36:46 +0000

    ci: measure individual add, delete, and bulk delete in stress tests
    [#173618082](https://www.pivotaltracker.com/story/show/173618082)
    
    Co-authored-by: Alex Standke <astandke@vmware.com>

--- routecontroller/stress/stress_test.go
+++ routecontroller/stress/stress_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"math"
 	"net/http"
 	"path/filepath"
 	"time"
@@ -21,6 +22,9 @@ import (
 type Results struct {
 	Time       int32     `json:"time"`
 	Add1000P95 []float64 `json:"bulk_add_1000_p95s"`
+	Add100P95  []float64 `json:"individually_add_100_p95s"`
+	Del100P95  []float64 `json:"individually_delete_100_p95s"`
+	Del1000P95 []float64 `json:"bulk_delete_1000_p95s"`
 }
 
 var _ = Describe("Stress Tests", func() {
@@ -41,7 +45,21 @@ var _ = Describe("Stress Tests", func() {
 			results = stressRouteController(numberOfRoutes, results)
 		}
 
+		fmt.Printf("Results: %+v", results)
+
 		for _, res := range results.Add1000P95 {
+			Expect(res).To(BeNumerically("<", 0.5))
+		}
+
+		for _, res := range results.Add100P95 {
+			Expect(res).To(BeNumerically("<", 0.25))
+		}
+
+		for _, res := range results.Del100P95 {
+			Expect(res).To(BeNumerically("<", 0.25))
+		}
+
+		for _, res := range results.Del1000P95 {
 			Expect(res).To(BeNumerically("<", 0.5))
 		}
 
@@ -77,95 +95,48 @@ func stressRouteController(numberOfRoutes int, results Results) Results {
 	// Wait for all the virtualservices to be created
 	Eventually(func() int { return kubectl.GetNumberOf("virtualservices") }, 30*time.Minute, 500*time.Millisecond).Should(Equal(numberOfRoutes))
 
-	resp, err := http.Get("http://localhost:30080/metrics")
-	Expect(err).NotTo(HaveOccurred())
-	metricsChan := make(chan *dto.MetricFamily, 1024)
-	err = prom2json.ParseResponse(resp, metricsChan)
-	Expect(err).NotTo(HaveOccurred())
+	add1000hist := getReconcileTime().Metric[0].Histogram
 
-	var reconcileTime *dto.MetricFamily
-
-	for metric := range metricsChan {
-		if metric.GetName() == "controller_runtime_reconcile_time_seconds" {
-			reconcileTime = metric
-		}
-	}
-
-	histogram := reconcileTime.Metric[0].Histogram
-
-	p95count := float32(histogram.GetSampleCount()) * float32(0.95)
-
-	var p95Add float64
-	for _, bucket := range histogram.Bucket {
+	p95count := float32(add1000hist.GetSampleCount()) * float32(0.95)
+	for _, bucket := range add1000hist.Bucket {
 		if float32(bucket.GetCumulativeCount()) > p95count {
-			p95Add = bucket.GetUpperBound()
+			results.Add1000P95 = append(results.Add1000P95, bucket.GetUpperBound())
 			break
 		}
 	}
 
-	results.Add1000P95 = append(results.Add1000P95, p95Add)
+	fmt.Println("Adding 100 routes one at a time")
+	for i := 0; i < 100; i++ {
+		route := buildSingleRoute(i, "the100")
+		session, err := kubectl.RunWithStdin(route, "apply", "-f", "-")
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(session).Should(gexec.Exit(0))
 
-	// TODO -- leaving these comments to help with the subsequent stories, make sure everything is
-	// gone before all the stress stories are complete
+		expectedNum := numberOfRoutes + i + 1
+		Expect(kubectl.GetNumberOf("routes")).To(Equal(expectedNum))
+		Eventually(func() int { return kubectl.GetNumberOf("virtualservices") }, 30*time.Minute, 500*time.Millisecond).Should(Equal(expectedNum))
+	}
 
-	//Expect(gbytes.BufferReader(resp.Body)).To(gbytes.Say("controller_runtime_reconcile"))
+	add100hist := getReconcileTime().Metric[0].Histogram
+	results.Add100P95 = append(results.Add100P95, findBucket(add100hist, add1000hist))
 
-	// addTime := timer(func() {
-	// 	Eventually(func() int { return kubectl.GetNumberOf("virtualservices") }, 30*time.Minute, 500*time.Millisecond).Should(Equal(numberOfRoutes))
-	// })
+	currentNumberOfRoutes := kubectl.GetNumberOf("routes")
+	fmt.Printf("Deleting 100 routes one at a time from the current %d routes\n", currentNumberOfRoutes)
+	for i := 0; i < 100; i++ {
+		route := buildSingleRoute(i, "the100")
+		session, err := kubectl.RunWithStdin(route, "delete", "-f", "-")
+		Expect(err).NotTo(HaveOccurred())
+		Eventually(session).Should(gexec.Exit(0))
 
-	// Expect(addTime.Seconds()).Should(BeNumerically("<", 120), "Should handle 1000 added routes in under 120 second")
-	// results.AddTimes = append(results.AddTimes, addTime.Seconds())
+		expectedNum := currentNumberOfRoutes - i - 1
+		Expect(kubectl.GetNumberOf("routes")).To(Equal(expectedNum))
+		Eventually(func() int { return kubectl.GetNumberOf("virtualservices") }, 30*time.Minute, 500*time.Millisecond).Should(Equal(expectedNum))
+	}
 
-	// fmt.Println("Adding 100 routes one at a time")
-	// times := []float64{}
-	// for i := 0; i < 100; i++ {
-	// 	route := buildSingleRoute(i, "the100")
-	// 	t := timer(func() {
-	// 		session, err := kubectl.RunWithStdin(route, "apply", "-f", "-")
-	// 		Expect(err).NotTo(HaveOccurred())
-	// 		Eventually(session).Should(gexec.Exit(0))
+	del100hist := getReconcileTime().Metric[0].Histogram
+	results.Del100P95 = append(results.Del100P95, findBucket(del100hist, add100hist))
 
-	// 		expectedNum := numberOfRoutes + i + 1
-	// 		Expect(kubectl.GetNumberOf("routes")).To(Equal(expectedNum))
-	// 		Eventually(func() int { return kubectl.GetNumberOf("virtualservices") }, 30*time.Minute, 500*time.Millisecond).Should(Equal(expectedNum))
-	// 	})
-
-	// 	times = append(times, t.Seconds())
-	// }
-	// mean, err := stats.Mean(times)
-	// Expect(err).NotTo(HaveOccurred())
-	// p95, err := stats.Percentile(times, 95)
-	// Expect(err).NotTo(HaveOccurred())
-	// results.Add100Means = append(results.Add100Means, mean)
-	// results.Add100P95s = append(results.Add100P95s, p95)
-	// currentNumberOfRoutes := kubectl.GetNumberOf("routes")
-
-	// fmt.Printf("Deleting 100 routes one at a time from the current %d routes\n", currentNumberOfRoutes)
-	// times = []float64{}
-	// for i := 0; i < 100; i++ {
-	// 	route := buildSingleRoute(i, "the100")
-	// 	t := timer(func() {
-	// 		session, err := kubectl.RunWithStdin(route, "delete", "-f", "-")
-	// 		Expect(err).NotTo(HaveOccurred())
-	// 		Eventually(session).Should(gexec.Exit(0))
-
-	// 		expectedNum := currentNumberOfRoutes - i - 1
-	// 		Expect(kubectl.GetNumberOf("routes")).To(Equal(expectedNum))
-	// 		Eventually(func() int { return kubectl.GetNumberOf("virtualservices") }, 30*time.Minute, 500*time.Millisecond).Should(Equal(expectedNum))
-	// 	})
-
-	// 	times = append(times, t.Seconds())
-	// }
-	// mean, err = stats.Mean(times)
-	// Expect(err).NotTo(HaveOccurred())
-	// p95, err = stats.Percentile(times, 95)
-	// Expect(err).NotTo(HaveOccurred())
-	// results.Delete100Means = append(results.Delete100Means, mean)
-	// results.Delete100P95s = append(results.Delete100P95s, p95)
-
-	// fmt.Printf("Deleting %d routes all at once\n", numberOfRoutes)
-	// deleteTime := timer(func() {
+	fmt.Printf("Deleting %d routes all at once\n", numberOfRoutes)
 	session, err = kubectl.Run("delete", "routes", "-l", "tag=initial", "--wait=false")
 	Expect(err).NotTo(HaveOccurred())
 	Eventually(session).Should(gexec.Exit(0))
@@ -177,14 +148,38 @@ func stressRouteController(numberOfRoutes int, results Results) Results {
 	Eventually(func() int {
 		return kubectl.GetNumberOf("virtualservices")
 	}, 30*time.Minute, 500*time.Millisecond).Should(Equal(0))
-	// })
 
-	// Expect(deleteTime.Seconds()).Should(BeNumerically("<", 120), "Should handle 1000 removed routes in under 120 seconds")
-	// results.DeleteTimes = append(results.DeleteTimes, deleteTime.Seconds())
+	del1000hist := getReconcileTime().Metric[0].Histogram
+	results.Del1000P95 = append(results.Del1000P95, findBucket(del1000hist, del100hist))
 
 	fmt.Println("Stress test complete, cleaning up...")
 	deleteRoutecontroller()
 	return results
+}
+
+func findBucket(current, previous *dto.Histogram) float64 {
+	p95count := float32(current.GetSampleCount()-previous.GetSampleCount()) * float32(0.95)
+	for i, bucket := range current.Bucket {
+		if float32(bucket.GetCumulativeCount()-previous.Bucket[i].GetCumulativeCount()) > p95count {
+			return bucket.GetUpperBound()
+		}
+	}
+	return math.Inf(1)
+}
+
+func getReconcileTime() *dto.MetricFamily {
+	resp, err := http.Get("http://localhost:30080/metrics")
+	Expect(err).NotTo(HaveOccurred())
+	metricsChan := make(chan *dto.MetricFamily, 1024)
+	err = prom2json.ParseResponse(resp, metricsChan)
+	Expect(err).NotTo(HaveOccurred())
+
+	for metric := range metricsChan {
+		if metric.GetName() == "controller_runtime_reconcile_time_seconds" {
+			return metric
+		}
+	}
+	return nil
 }
 
 func setupRoutes(numberOfRoutes int, tag string) {
