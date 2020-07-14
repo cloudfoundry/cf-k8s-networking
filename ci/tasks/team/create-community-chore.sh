7a14b4aba566f0cea605e95609cef313f2a0cb64
Jen Spinney <jspinney@pivotal.io>
2020-07-14 20:51:55 +0000

    ci: make xargs parameters match busybox's xargs command
    Co-authored-by: Jen Spinney <jspinney@pivotal.io>

--- ci/tasks/team/create-community-chore.sh
+++ ci/tasks/team/create-community-chore.sh
@@ -13,6 +13,6 @@ template_tasks=`curl -s -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.
 
 echo "Creating story..."
 story_id=`curl -s -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d "$template_story" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories" | jq .id`
-echo $template_tasks | jq -c '(.[])' | xargs -L1 -d$'\n' -I{} curl -s -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d '{}' "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/$story_id/tasks" > /dev/null
+echo $template_tasks | jq -c '(.[])' | xargs -n1 -I{} curl -s -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d '{}' "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/$story_id/tasks" > /dev/null
 
 echo "Created Story id $story_id"
