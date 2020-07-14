c01dcd754823bbedf0d243f391a5c3b8b9bfdc21
Christian Ang <angc@vmware.com>
2020-07-14 18:22:14 +0000

    Add flag to create-gke-cluster for auto teardown
    [#173753544](https://www.pivotaltracker.com/story/show/173753544)
    
    Co-authored-by: Tim Downey <tdowney@vmware.com>

--- ci/tasks/cf4k8s/create-gke-cluster.sh
+++ ci/tasks/cf4k8s/create-gke-cluster.sh
@@ -12,33 +12,37 @@ set -euo pipefail
 : "${GCP_PROJECT:?}"
 : "${MACHINE_TYPE:?}"
 : "${NUM_NODES:?}"
+: "${ENABLE_AUTOMATIC_TEARDOWN_AFTER_24_HOURS:?}"
 
 function latest_cluster_version() {
   gcloud container get-server-config --zone us-west1-a 2>/dev/null | yq .validMasterVersions[0] -r
 }
 
 function create_cluster() {
-    gcloud auth activate-service-account --key-file=<(echo "${GCP_SERVICE_ACCOUNT_KEY}") --project="${GCP_PROJECT}" 1>/dev/null 2>&1
+  gcloud auth activate-service-account --key-file=<(echo "${GCP_SERVICE_ACCOUNT_KEY}") --project="${GCP_PROJECT}" 1>/dev/null 2>&1
 
-    if gcloud container clusters describe ${CLUSTER_NAME} > /dev/null; then
-        echo "${CLUSTER_NAME} already exists! Destroying..."
-        gcloud container clusters delete ${CLUSTER_NAME} --quiet
-    fi
+  if gcloud container clusters describe ${CLUSTER_NAME} > /dev/null; then
+    echo "${CLUSTER_NAME} already exists! Destroying..."
+    gcloud container clusters delete ${CLUSTER_NAME} --quiet
+  fi
 
 
-    additional_args=()
-    if [ "${ENABLE_IP_ALIAS}" = true ]; then
-        additional_args+=("--enable-ip-alias")
-    fi
+  additional_args=()
+  if [[ "${ENABLE_IP_ALIAS}" = true ]]; then
+    additional_args+=("--enable-ip-alias")
+  fi
+  if [[ "${ENABLE_AUTOMATIC_TEARDOWN_AFTER_24_HOURS}" = true ]]; then
+    additional_args+=("--max-age=24h")
+  fi
 
-    echo "Creating cluster: ${CLUSTER_NAME} ..."
-    gcloud container clusters create ${CLUSTER_NAME} \
-        --cluster-version=$(latest_cluster_version) \
-        --machine-type=${MACHINE_TYPE} \
-        --labels team=cf-k8s-networking-ci \
-        --enable-network-policy \
-        --num-nodes "${NUM_NODES}" \
-        "${additional_args[@]}"
+  echo "Creating cluster: ${CLUSTER_NAME} ..."
+  gcloud container clusters create ${CLUSTER_NAME} \
+    --cluster-version=$(latest_cluster_version) \
+    --machine-type=${MACHINE_TYPE} \
+    --labels team=cf-k8s-networking-ci \
+    --enable-network-policy \
+    --num-nodes "${NUM_NODES}" \
+    "${additional_args[@]}"
 }
 
 function main() {
