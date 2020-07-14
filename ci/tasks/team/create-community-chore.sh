aac5295b13a8afdc9c62c62222e829572fe44794
Alex Standke <astandke@vmware.com>
2020-07-14 19:23:35 +0000

    ci: add task to create community chore daily
    [#173799637](https://www.pivotaltracker.com/story/show/173799637)
    
    Co-authored-by: Jen Spinney <jspinney@pivotal.io>

---
+++ ci/tasks/team/create-community-chore.sh
@@ -0,0 +1,13 @@
+set -euo pipefail
+
+project_id=2407973
+
+echo "Fetching Template Story..."
+template_story=`curl -s -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/173205458" | sed -E "s|YYYY/MM/DD|$(date '+%Y/%m/%d')|"`
+template_tasks=`curl -s -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/173205458/tasks"`
+
+echo "Creating story..."
+story_id=`curl -s -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d "$template_story" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories" | jq .id`
+echo $template_tasks | jq -c '(.[])' | xargs -L1 -d$'\n' -I{} curl -s -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d '{}' "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/$story_id/tasks" > /dev/null
+
+echo "Created Story id $story_id"
