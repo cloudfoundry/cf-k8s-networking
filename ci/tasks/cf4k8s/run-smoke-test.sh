7e27a45eecf0074610404ac4c179c4f7d15a02af
Christian Ang <angc@vmware.com>
2020-07-15 18:58:25 +0000

    Add flake attempts to the gke tests
    - this is being temporarily set because there are intemittent issues
    with cf pushing apps. Reference https://github.com/cloudfoundry/cf-for-k8s/issues/255
    
    [#173820343](https://www.pivotaltracker.com/story/show/173820343)
    
    Co-authored-by: Jen Spinney <jspinney@pivotal.io>

--- ci/tasks/cf4k8s/run-smoke-test.sh
+++ ci/tasks/cf4k8s/run-smoke-test.sh
@@ -2,13 +2,18 @@
 
 set -euo pipefail
 
+ROOT="$(cd "$(dirname "${0}")/../../../.." && pwd)"
+
+FLAKE_ATTEMPTS="${FLAKE_ATTEMPTS:-0}"
+
 function run_smoke_test() {
     DNS_DOMAIN=$(cat env-metadata/dns-domain.txt)
     export SMOKE_TEST_API_ENDPOINT="https://api.${DNS_DOMAIN}"
     export SMOKE_TEST_APPS_DOMAIN="apps.${DNS_DOMAIN}"
     export SMOKE_TEST_USERNAME=admin
     export SMOKE_TEST_PASSWORD=$(cat env-metadata/cf-admin-password.txt)
-    cf-for-k8s/hack/run-smoke-tests.sh
+    cd "${ROOT}/cf-for-k8s/tests/smoke"
+    ginkgo -v -flakeAttempts="${FLAKE_ATTEMPTS}" ./
 }
 
 function main() {
