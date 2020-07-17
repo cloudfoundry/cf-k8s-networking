00bbefc47e93074b9ba4a63a441029d3287f8d12
Jen Spinney <jspinney@pivotal.io>
2020-07-17 23:02:04 +0000

    WIP: Add community chore to top of backlog
    Co-authored-by: Christian Ang <angc@vmware.com>

--- ci/tasks/team/create-community-chore.sh
+++ ci/tasks/team/create-community-chore.sh
@@ -8,11 +8,16 @@ set -euo pipefail
 project_id=2407973
 
 echo "Fetching Template Story..."
-template_story=`curl -s -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/173205458" | sed -E "s|YYYY/MM/DD|$(date '+%Y/%m/%d')|"`
-template_tasks=`curl -s -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/173205458/tasks"`
+template_story=`curl -s -f -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/173205458"`
+template_tasks=`curl -s -f -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/173205458/tasks"`
+
+top_of_backlog_story_id=`curl -s -f -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories?with_state=unstarted&limit=1" | jq '.[0].id'`
 
 echo "Creating story..."
-story_id=`curl -s -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d "$template_story" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories" | jq .id`
-echo $template_tasks | jq -c '(.[])' | xargs -n1 -I{} curl -s -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d '{}' "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/$story_id/tasks" > /dev/null
+new_story=`echo $template_story | sed -E "s#YYYY/MM/DD#$(date '+%Y/%m/%d')#" | jq ". + {\"before_id\": $top_of_backlog_story_id }"`
+echo $new_story
+story_id=`curl -f -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d "$new_story" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories" | jq .id`
+echo $story_id
+echo $template_tasks | jq -c '(.[])' | xargs -n1 -I{} curl -s -f -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d '{}' "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/$story_id/tasks" > /dev/null
 
 echo "Created Story id $story_id"
