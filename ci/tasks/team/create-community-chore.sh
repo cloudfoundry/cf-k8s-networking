f254b4e3d31ab3bd1afbc02423e3f5b81d0d0a4a
Alex Standke <astandke@vmware.com>
2020-07-28 19:07:09 +0000

    chore: update create-community-chore to put it at the top of the backlog
    Co-authored-by: Tim Downey <tdowney@vmware.com>

--- ci/tasks/team/create-community-chore.sh
+++ ci/tasks/team/create-community-chore.sh
@@ -7,12 +7,14 @@ set -euo pipefail
 
 project_id=2407973
 
-echo "Fetching Template Story..."
-template_story=`curl -s -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/173205458" | sed -E "s|YYYY/MM/DD|$(date '+%Y/%m/%d')|"`
+# echo "Fetching Template Story..."
+template_story=`curl -s -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/173205458" | sed -E "s|YYYY/MM/DD|$(date '+%Y/%m/%d')|" | jq '. + {"current_state": "started"}'`
 template_tasks=`curl -s -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/173205458/tasks"`
 
 echo "Creating story..."
+
 story_id=`curl -s -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d "$template_story" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories" | jq .id`
+curl -s -X PUT -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d '{"current_state":"unstarted"}' "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/$story_id" > /dev/null
 echo $template_tasks | jq -c '(.[])' | xargs -n1 -I{} curl -s -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d '{}' "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/$story_id/tasks" > /dev/null
 
 echo "Created Story id $story_id"
