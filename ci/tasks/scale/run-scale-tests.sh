eef47327af8aaf74e1291328183b0ac71c33dee1
Tim Downey <tdowney@vmware.com>
2020-07-30 23:37:23 +0000

    remove cf-target from scale test script

--- ci/tasks/scale/run-scale-tests.sh
+++ ci/tasks/scale/run-scale-tests.sh
@@ -5,10 +5,6 @@ set -euo pipefail
 function login_and_target() {
     cf api --skip-ssl-validation "https://api.$(cat env-metadata/dns-domain.txt)"
     CF_USERNAME=admin CF_PASSWORD=$(cat env-metadata/cf-admin-password.txt) cf auth
-
-    export ORG_NAME="scale-tests"
-    export SPACE_NAME="${ORG_NAME}"
-    cf target -o "${ORG_NAME}" -s "${SPACE_NAME}"
 }
 
 function run_scale_test() {
