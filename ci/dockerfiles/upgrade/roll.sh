cbade83a3c344918c1b580e1ebedb0a6d986c345
Tim Downey <tdowney@vmware.com>
2020-08-03 22:17:38 +0000

    no longer wait for configmaps in upgrade job
    - some deployments will have more or fewer configmaps depending on what
    they deploy
    - it was causing problems when prometheus was enabled
    
    [#173346213](https://www.pivotaltracker.com/story/show/173346213)
    
    Co-authored-by: Alex Standke <astandke@vmware.com>

--- ci/dockerfiles/upgrade/roll.sh
+++ ci/dockerfiles/upgrade/roll.sh
@@ -2,9 +2,8 @@
 
 while true; do
   sleep 1
-  configmaps=$(kubectl get configmaps -n istio-system -l "cloudfoundry.org/istio_version=$ISTIO_VERSION" --no-headers | wc -l)
   deposets=$(kubectl get daemonsets,pods,deployments -n istio-system -l "cloudfoundry.org/istio_version notin ($ISTIO_VERSION)" | wc -l)
-  if [[ $configmaps == 2 && $deposets == 0 ]]; then
+  if [[ $deposets == 0 ]]; then
     break
   fi
   echo "Didn't quite find it this time... will try again in a sec"
