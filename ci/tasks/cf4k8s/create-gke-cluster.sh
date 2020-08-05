8777e0b385c90c617850d0a9d72f9b0339c0060e
Clay Kauzlaric <ckauzlaric@vmware.com>
2020-08-05 00:04:39 +0000

    fix for regional cluster deleting
    Co-authored-by: Alex Standke <astandke@vmware.com>

--- ci/tasks/cf4k8s/create-gke-cluster.sh
+++ ci/tasks/cf4k8s/create-gke-cluster.sh
@@ -21,13 +21,6 @@ function latest_cluster_version() {
 
 function create_cluster() {
     gcloud auth activate-service-account --key-file=<(echo "${GCP_SERVICE_ACCOUNT_KEY}") --project="${GCP_PROJECT}" 1>/dev/null 2>&1
-
-    if gcloud container clusters describe ${CLUSTER_NAME} > /dev/null; then
-        echo "${CLUSTER_NAME} already exists! Destroying..."
-        gcloud container clusters delete ${CLUSTER_NAME} --quiet
-    fi
-
-
     additional_args=()
     if [ "${ENABLE_IP_ALIAS}" = true ]; then
         additional_args+=("--enable-ip-alias")
@@ -37,6 +30,12 @@ function create_cluster() {
         additional_args+=("--region")
         additional_args+=("${CLOUDSDK_COMPUTE_REGION}")
     fi
+
+    if gcloud container clusters describe ${CLUSTER_NAME} > /dev/null; then
+        echo "${CLUSTER_NAME} already exists! Destroying..."
+        gcloud container clusters delete ${CLUSTER_NAME} --quiet "${additional_args[@]}"
+    fi
+
 
     echo "Creating cluster: ${CLUSTER_NAME} ..."
     gcloud container clusters create ${CLUSTER_NAME} \
