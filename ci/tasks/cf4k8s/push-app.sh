8e6bf439a67b595a60064fca81d894a2aa1ea32d
Kauana dos Santos <kdossantos@vmware.com>
2020-08-25 21:07:29 +0000

    add retries to push-apps
    - sometimes it fails, seemingly because the cf isn't ready yet
    
    [#174366108](https://www.pivotaltracker.com/story/show/174366108)
    
    Co-authored-by: Alex Standke <astandke@vmware.com>

--- ci/tasks/cf4k8s/push-app.sh
+++ ci/tasks/cf4k8s/push-app.sh
@@ -14,27 +14,43 @@ function target_cf() {
     local cf_domain=$(cat "${ROOT}/cf-install-values/cf-install-values.yml" | \
         grep system_domain | awk '{print $2}' | tr -d '"')
 
-    cf api --skip-ssl-validation "https://api.${cf_domain}"
+    cf_with_retry api --skip-ssl-validation "https://api.${cf_domain}"
     local password=$(cat "${ROOT}/cf-install-values/cf-install-values.yml" | \
         grep cf_admin_password | awk '{print $2}')
-    cf auth "admin" "${password}"
+    cf_with_retry auth "admin" "${password}"
 }
 
 function create_org_and_space() {
-    cf create-org "${ORG_NAME}"
-    cf create-space -o "${ORG_NAME}" "${SPACE_NAME}"
+    cf_with_retry create-org "${ORG_NAME}"
+    cf_with_retry create-space -o "${ORG_NAME}" "${SPACE_NAME}"
 }
 
 function deploy_app() {
     local name="${1}"
-    cf push "${name}" -o "cfrouting/httpbin" -i "${INSTANCES}"
+    cf_with_retry push "${name}" -o "cfrouting/httpbin" -i "${INSTANCES}"
+}
+
+function cf_with_retry() {
+    cf_command=$*
+
+    set +euo pipefail
+
+    for i in {1..3}
+    do
+        echo "Running cf ${cf_command}..."
+        cf $cf_command && set -euo pipefail && return
+        sleep 10
+    done
+
+    echo "cf_with_retry command has failed 3 times"
+    exit
 }
 
 function main() {
     target_cf
     create_org_and_space
-    cf target -o "${ORG_NAME}" -s "${SPACE_NAME}"
-    cf enable-feature-flag diego_docker
+    cf_with_retry target -o "${ORG_NAME}" -s "${SPACE_NAME}"
+    cf_with_retry enable-feature-flag diego_docker
     deploy_app "${APP_NAME}"
 }
 
