984ad803e6be80899db6177155df49fae663edf2
Clay Kauzlaric <ckauzlaric@vmware.com>
2020-08-27 23:11:09 +0000

    ci: annotate task can use image.tar or image name
    * deplab can use image.tar files or just the image repository name. We
    wanted our task to be able to do both
    
    [#174478455](https://www.pivotaltracker.com/story/show/174478455)
    
    Co-authored-by: Jen Spinney <jspinney@pivotal.io>

--- ci/tasks/docker/annotate.sh
+++ ci/tasks/docker/annotate.sh
@@ -2,7 +2,15 @@
 
 set -euo pipefail
 
-deplab --image-tar input-image/image.tar \
-  --git repository \
-  --output-tar output-image/image.tar
+if [[ -d input-image-tar ]]; then
+  deplab --image-tar input-image-tar/image.tar \
+    --git repository \
+    --output-tar output-image/image.tar
+elif [[ -d input-image-name ]]; then
+  deplab --image "$(cat input-image-name/name.txt)" \
+    --git repository \
+    --output-tar output-image/image.tar
+else
+      echo "When using this task, you must specify EITHER input-image-tar OR input-image-name"
+fi
 
