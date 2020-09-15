522f6f02fee4e3cacaaecd885fb335db473a21e5
Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
2020-09-15 21:26:57 +0000

    ci: remove tasks from story creation
    Co-authored-by: Clay Kauzlaric <ckauzlaric@vmware.com>

--- ci/tasks/team/create-istio-bump-story.sh
+++ ci/tasks/team/create-istio-bump-story.sh
@@ -16,7 +16,6 @@ echo "Creating story..."
 
 story_id=`curl -s -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d "$template_story" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories" | jq .id`
 curl -s -X PUT -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d '{"current_state":"unstarted"}' "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/$story_id" > /dev/null
-echo $template_tasks | jq -c '(.[])' | xargs -n1 -I{} curl -s -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d '{}' "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/$story_id/tasks" > /dev/null
 
 echo "Created Story id $story_id"
 
