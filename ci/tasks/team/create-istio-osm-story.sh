cbde0bced22fe4b5b667d3ffba0d08e0f3a6d504
Clay Kauzlaric <ckauzlaric@vmware.com>
2020-09-16 21:21:59 +0000

    ci: add job to create istio licensing story
    [#174536204](https://www.pivotaltracker.com/story/show/174536204)
    
    Co-authored-by: Kauana dos Santos <kdossantos@vmware.com>

---
+++ ci/tasks/team/create-istio-osm-story.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# ENV
+: "${TRACKER_TOKEN:?}"
+
+project_id=2382853
+
+ISTIO_VERSION="$(cat istio-release/tag)"
+
+# echo "Fetching Template Story..."
+template_story=`curl -s -X GET -H "X-TrackerToken: $TRACKER_TOKEN" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/174851272" | sed -E "s|X\.X\.X|${ISTIO_VERSION}|" | jq '. + {"current_state": "started"}'`
+
+echo "Creating story..."
+
+story_id=`curl -s -X POST -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d "$template_story" "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories" | jq .id`
+curl -s -X PUT -H "X-TrackerToken: $TRACKER_TOKEN" -H "Content-Type: application/json" -d '{"current_state":"unstarted"}' "https://www.pivotaltracker.com/services/v5/projects/$project_id/stories/$story_id" > /dev/null
+
+echo "Created Story id $story_id"
+
+if [[ $story_id == "null" ]]; then
+    exit 1
+fi
