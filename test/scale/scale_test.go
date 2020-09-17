7bbee147264392812319f6d7ddd04183d1a490a9
Leah Hanson <lhanson@pivotal.io>
2020-09-17 22:07:48 +0000

    scale: all the changes for the scaling pipeline
    We've seen the tests run through with P95 of 35s.
    
    - push apps to separate spaces for scaling test
    since most of Cloud Controller's resources are scopes by space
    and putting 1000 apps in a single space will have performance
    consequences for the API
    
    - use proxy app instead of httpbin for scale tests since
    proxy is a golang/alpine linux based app which has a lighter footprint
    than the python/ubuntu based httpbin image; it should `cf push` a bit
    faster
    
    - also add a small sleep between each `cf push` so that we don't spam
    the k8s API with map route requests
    
    - use regional cluster for scaling tests since
    the k8s control-plane node on the zonal cluster would go offline at times
    zonal clusters have a highly available control plane which might help here
    
    - there are two kinds of post-success map-route failures: transient and
    persistent. We ignore transient failures; a failure is transient if it
    later resolves (later requests return 200). The expectation is that
    transient failures happen because we make a request to a gateway while
    it is in the midst of being configured by Istio. For persistent
    failures, which are failures that never become 200s again, we count the
    failures, print the count in the summary, and fail if any occurred.
    
    - use 10 apps per space so that CAPI isn't overwhelmed
    
    [#171678788](https://www.pivotaltracker.com/story/show/171678788)
    
    Co-authored-by: Leah Hanson <lhanson@pivotal.io>
    Co-authored-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
    Co-authored-by: Alex Standke <astandke@vmware.com>
    Co-authored-by: Kauana dos Santos <kdossantos@vmware.com>
    Co-authored-by: Mikael Manukyan <mmanukyan@vmware.com>
    Co-authored-by: Clay Kauzlaric <ckauzlaric@vmware.com>
    Co-authored-by: Tim Downey <tdowney@vmware.com>
    Co-authored-by: Mikael Manukyan <mmanukyan@vmware.com>
    Co-authored-by: Clay Kauzlaric <ckauzlaric@vmware.com>
    Co-authored-by: Leah Hanson <lhanson@pivotal.io>
    Co-authored-by: Nitya Dhanushkodi <ndhanushkodi@pivotal.io>
    Co-authored-by: Alex Standke <astandke@vmware.com>

--- test/scale/scale_test.go
+++ test/scale/scale_test.go
@@ -1,6 +1,8 @@
 package scale_test
 
 import (
+	"crypto/tls"
+	"math"
 	"net/http"
 	"time"
 
@@ -18,14 +20,21 @@ import (
 
 var _ = Describe("Scale", func() {
 	var (
-		routeMapper *collector.RouteMapper
-		results     []float64
+		routeMapper  *collector.RouteMapper
+		results      []float64
+		failures     int
+		postFailures int
 	)
 
 	BeforeEach(func() {
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+
 		routeMapper = &collector.RouteMapper{
 			Client: http.Client{
-				Timeout: 1 * time.Second,
+				Timeout:   1 * time.Second,
+				Transport: tr,
 			},
 		}
 	})
@@ -34,7 +43,9 @@ var _ = Describe("Scale", func() {
 		// For development purposes, to reset the routes back to the original hostnames
 		// so we can rerun the tests
 		if cleanup {
-			for i := 0; i < numApps; i++ {
+			fmt.Fprintln(GinkgoWriter, "Beginning cleanup...")
+			forEachAppInSpace(numApps, numAppsPerSpace, func(i int) {
+				fmt.Fprintln(GinkgoWriter, i, "of", numApps)
 				appName := fmt.Sprintf("bin-%d", i)
 				routeHost := fmt.Sprintf("bin-new-%d", i)
 
@@ -43,40 +54,65 @@ var _ = Describe("Scale", func() {
 
 				session = cf.Cf("map-route", appName, domain, "--hostname", appName)
 				Eventually(session, "30s").Should(Exit(0))
-			}
-
-			// Print out the statistics after the test
-			p95, _ := stats.Percentile(results, 95)
-			min, _ := stats.Min(results)
-			max, _ := stats.Max(results)
-			avg, _ := stats.Mean(results)
-			fmt.Fprintln(GinkgoWriter, "\n\n\n*********************************************")
-			fmt.Fprintln(GinkgoWriter, "Map Route Latency Steady State Results")
-			fmt.Fprintf(GinkgoWriter, "\tP95: %.2f Seconds\n", p95)
-			fmt.Fprintf(GinkgoWriter, "\tMin: %.2f Seconds\n", min)
-			fmt.Fprintf(GinkgoWriter, "\tMax: %.2f Seconds\n", max)
-			fmt.Fprintf(GinkgoWriter, "\tAverage: %.2f Seconds\n", avg)
-			fmt.Fprintln(GinkgoWriter, "*********************************************")
+			})
 		}
+
+		// Print out the statistics after the test
+		p95, _ := stats.Percentile(results, 95)
+		min, _ := stats.Min(results)
+		max, _ := stats.Max(results)
+		median, _ := stats.Median(results)
+		avg, _ := stats.Mean(results)
+		fmt.Fprintln(GinkgoWriter, "\n\n\n*********************************************")
+		fmt.Fprintln(GinkgoWriter, "Map Route Latency Steady State Results")
+		fmt.Fprintf(GinkgoWriter, "\tP95: %.0f Seconds\n", p95)
+		fmt.Fprintf(GinkgoWriter, "\tMin: %.0f Seconds\n", min)
+		fmt.Fprintf(GinkgoWriter, "\tMax: %.0f Seconds\n", max)
+		fmt.Fprintf(GinkgoWriter, "\tAverage: %.0f Seconds\n", avg)
+		fmt.Fprintf(GinkgoWriter, "\tMedian: %.0f Seconds\n", median)
+		fmt.Fprintf(GinkgoWriter, "\n\tRoutes failed to map: %d\n", failures)
+		fmt.Fprintf(GinkgoWriter, "\n\tPost-success failures: %d\n", postFailures)
+		fmt.Fprintln(GinkgoWriter, "*********************************************")
 	})
 
 	Context("On an environment with 1000 apps and 1000 routes", func() {
 		It("maps 95% of the routes within 10 seconds", func() {
-			for i := 0; i < numApps; i++ {
+			forEachAppInSpace(numApps, numAppsPerSpace, func(i int) {
+				fmt.Fprintln(GinkgoWriter, "Handling app", i)
 				appName := fmt.Sprintf("bin-%d", i)
 				routeToDelete := fmt.Sprintf("bin-%d", i)
 				routeToMap := fmt.Sprintf("bin-new-%d", i)
 				routeMapper.MapRoute(appName, domain, routeToDelete, routeToMap)
-				time.Sleep(10 * time.Second)
-			}
+				time.Sleep(15 * time.Second)
+			})
 
 			routeMapper.Wait()
 
 			results = routeMapper.GetResults()
+			failures = routeMapper.GetFailures()
+			postFailures = routeMapper.GetPostFailures()
 			p95, err := stats.Percentile(results, 95)
 			Expect(err).NotTo(HaveOccurred())
 
+			Expect(postFailures).To(Equal(0), "Expected no routes to succeed, then fail and never recover")
+			Expect(failures).To(Equal(0), "Expected no map-routes to fail but some did :(")
 			Expect(p95).To(BeNumerically("<=", 10))
 		})
 	})
 })
+
+func forEachAppInSpace(apps, appsPerSpace int, f func(int)) {
+	numOrgsSpaces := int(math.Ceil(float64(apps) / float64(appsPerSpace)))
+	for n := 0; n < numOrgsSpaces; n++ {
+		fmt.Fprintf(GinkgoWriter, "############## Apps: %d, AppsPerSpace: %d, NumOrgSpaces: %d ##############\n", apps, appsPerSpace, numOrgsSpaces)
+		fmt.Fprintln(GinkgoWriter, "Targeting org and space ", n)
+		session := cf.Cf("target", "-o", fmt.Sprintf("%s-%d", orgNamePrefix, n), "-s", fmt.Sprintf("%s-%d", spaceNamePrefix, n))
+		Eventually(session, "30s").Should(Exit(0))
+
+		for i := 0; i < int(math.Min(float64(appsPerSpace), float64(apps))); i++ {
+			appNumber := (n * appsPerSpace) + i
+			f(appNumber)
+		}
+		time.Sleep(30 * time.Second)
+	}
+}
