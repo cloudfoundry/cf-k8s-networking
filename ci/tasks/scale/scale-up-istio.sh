fe63985ab885f15ef36b2811157995adde197f03
Kauana dos Santos <kdossantos@vmware.com>
2020-09-21 18:48:49 +0000

    wip

---
+++ ci/tasks/scale/scale-up-istio.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# move cf-for-k8s to output dir
+cp -r cf-for-k8s scaled-cf-for-k8s
+
+# run ytt on istioctl-values and on values
+ytt -f cf-k8s-networking-ci/tasks/scale/istioctl-values-overlay.yml -f cf-for-k8s/build/istio/istioctl-values.yaml > cf-for-k8s-scaled/build/istio/istioctl-values.yaml
+ytt -f cf-k8s-networking-ci/tasks/scale/istio-values-overlay.yml -f cf-for-k8s/build/istio/values.yaml > cf-for-k8s-scaled/build/istio/values.yaml
+
+# generate new XXX files
+scaled-cf-for-k8s/build/istio/build.sh
