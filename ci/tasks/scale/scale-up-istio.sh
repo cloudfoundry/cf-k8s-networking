4a9a29268199b2fa202134d80de217e633d56560
Kauana dos Santos <kdossantos@vmware.com>
2020-09-21 18:56:17 +0000

    Make the name cf-for-k8s-scaled everywhere.

--- ci/tasks/scale/scale-up-istio.sh
+++ ci/tasks/scale/scale-up-istio.sh
@@ -3,11 +3,11 @@
 set -euo pipefail
 
 # move cf-for-k8s to output dir
-cp -r cf-for-k8s scaled-cf-for-k8s
+cp -r cf-for-k8s cf-for-k8s-scaled
 
 # run ytt on istioctl-values and on values
 ytt -f cf-k8s-networking-ci/tasks/scale/istioctl-values-overlay.yml -f cf-for-k8s/build/istio/istioctl-values.yaml > cf-for-k8s-scaled/build/istio/istioctl-values.yaml
 ytt -f cf-k8s-networking-ci/tasks/scale/istio-values-overlay.yml -f cf-for-k8s/build/istio/values.yaml > cf-for-k8s-scaled/build/istio/values.yaml
 
 # generate new XXX files
-scaled-cf-for-k8s/build/istio/build.sh
+cf-for-k8s-scaled/build/istio/build.sh
