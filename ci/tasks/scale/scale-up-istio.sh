f21d28018ddcb072d3fb70621975d703a863e4a9
Kauana dos Santos <kdossantos@vmware.com>
2020-09-21 21:48:29 +0000

    Give up on ytt for value.yaml file.

--- ci/tasks/scale/scale-up-istio.sh
+++ ci/tasks/scale/scale-up-istio.sh
@@ -7,7 +7,7 @@ cp -r cf-for-k8s cf-for-k8s-scaled
 
 # run ytt on istioctl-values and on values
 ytt -f cf-k8s-networking-ci/ci/tasks/scale/istioctl-values-overlay.yml -f cf-for-k8s/build/istio/istioctl-values.yaml > cf-for-k8s-scaled/build/istio/istioctl-values.yaml
-ytt -f cf-k8s-networking-ci/ci/tasks/scale/istio-values-overlay.yml -f cf-for-k8s/build/istio/values.yaml > cf-for-k8s-scaled/build/istio/values.yaml
+mv cf-k8s-networking-ci/ci/tasks/scale/istio-values.yml cf-for-k8s-scaled/build/istio/values.yaml
 
 # generate new XXX files
 cf-for-k8s-scaled/build/istio/build.sh
