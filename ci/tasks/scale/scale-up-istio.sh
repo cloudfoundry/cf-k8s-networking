00bfa8840225fd771806d02a39fee0b833a281d2
Kauana dos Santos <kdossantos@vmware.com>
2020-09-21 19:02:24 +0000

    Correct path to tasks/scale

--- ci/tasks/scale/scale-up-istio.sh
+++ ci/tasks/scale/scale-up-istio.sh
@@ -6,8 +6,8 @@ set -euo pipefail
 cp -r cf-for-k8s cf-for-k8s-scaled
 
 # run ytt on istioctl-values and on values
-ytt -f cf-k8s-networking-ci/tasks/scale/istioctl-values-overlay.yml -f cf-for-k8s/build/istio/istioctl-values.yaml > cf-for-k8s-scaled/build/istio/istioctl-values.yaml
-ytt -f cf-k8s-networking-ci/tasks/scale/istio-values-overlay.yml -f cf-for-k8s/build/istio/values.yaml > cf-for-k8s-scaled/build/istio/values.yaml
+ytt -f cf-k8s-networking-ci/ci/tasks/scale/istioctl-values-overlay.yml -f cf-for-k8s/build/istio/istioctl-values.yaml > cf-for-k8s-scaled/build/istio/istioctl-values.yaml
+ytt -f cf-k8s-networking-ci/ci/tasks/scale/istio-values-overlay.yml -f cf-for-k8s/build/istio/values.yaml > cf-for-k8s-scaled/build/istio/values.yaml
 
 # generate new XXX files
 cf-for-k8s-scaled/build/istio/build.sh
