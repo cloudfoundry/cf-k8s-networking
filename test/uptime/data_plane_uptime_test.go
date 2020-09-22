95a5e396cda116b4735feef9a9818510783ffd11
Kauana dos Santos <kdossantos@vmware.com>
2020-09-22 23:38:46 +0000

    comment out data plane test

--- test/uptime/data_plane_uptime_test.go
+++ test/uptime/data_plane_uptime_test.go
@@ -1,70 +1,64 @@
 package uptime_test
 
 import (
-	"fmt"
-	"net/http"
-	"time"
-
-	"code.cloudfoundry.org/cf-k8s-networking/test/uptime/internal/checker"
-	"code.cloudfoundry.org/cf-k8s-networking/test/uptime/internal/uptime"
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
+	// . "github.com/onsi/gomega"
 )
 
 var _ = Describe("Data Plane Uptime", func() {
 	var (
-		results        *uptime.DataPlaneResults
-		upgradeChecker *checker.Upgrade
-		startTime      time.Time
+	// results        *uptime.DataPlaneResults
+	// upgradeChecker *checker.Upgrade
+	// startTime      time.Time
 	)
 
 	BeforeEach(func() {
-		results = &uptime.DataPlaneResults{}
-		upgradeChecker = &checker.Upgrade{
-			PollInterval: 1 * time.Second,
-		}
-		upgradeChecker.Start()
+		// results = &uptime.DataPlaneResults{}
+		// upgradeChecker = &checker.Upgrade{
+		// PollInterval: 1 * time.Second,
+		// }
+		// upgradeChecker.Start()
 
-		startTime = time.Now()
+		// startTime = time.Now()
 	})
 
 	AfterEach(func() {
-		upgradeChecker.Stop()
+		// upgradeChecker.Stop()
 	})
 
 	It("measures the data plane uptime", func() {
 		By("checking whether X% of requests are successful within the acceptable response time during an upgrade", func() {
-			for {
-				if !upgradeChecker.HasFoundUpgrade() && time.Since(startTime) > upgradeDiscoveryTimeout {
-					Fail(fmt.Sprintf("failed to find cf upgrade in %s", upgradeDiscoveryTimeout.String()))
-				}
+			// for {
+			// 	if !upgradeChecker.HasFoundUpgrade() && time.Since(startTime) > upgradeDiscoveryTimeout {
+			// 		Fail(fmt.Sprintf("failed to find cf upgrade in %s", upgradeDiscoveryTimeout.String()))
+			// 	}
 
-				// if the upgrade is finished (learned by checking the "finished at" in
-				// kapp app-change ls), and we've run for at least 15 minutes, stop running the test
-				if upgradeChecker.HasFoundUpgrade() && upgradeChecker.IsUpgradeFinished() {
-					break
-				}
+			// 	// if the upgrade is finished (learned by checking the "finished at" in
+			// 	// kapp app-change ls), and we've run for at least 15 minutes, stop running the test
+			// 	if upgradeChecker.HasFoundUpgrade() && upgradeChecker.IsUpgradeFinished() {
+			// 		break
+			// 	}
 
-				resp, err, requestLatency := timeGetRequest(dataPlaneSLIAppRouteURL)
-				if err != nil {
-					results.RecordError(err)
-					continue
-				}
+			// 	resp, err, requestLatency := timeGetRequest(dataPlaneSLIAppRouteURL)
+			// 	if err != nil {
+			// 		results.RecordError(err)
+			// 		continue
+			// 	}
 
-				hasStatusOK := resp.StatusCode == http.StatusOK
-				hasMetRequestLatencySLO := requestLatency < dataPlaneSLOMaxRequestLatency
-				hasPassedSLI := hasStatusOK && hasMetRequestLatencySLO
+			// 	hasStatusOK := resp.StatusCode == http.StatusOK
+			// 	hasMetRequestLatencySLO := requestLatency < dataPlaneSLOMaxRequestLatency
+			// 	hasPassedSLI := hasStatusOK && hasMetRequestLatencySLO
 
-				results.Record(hasPassedSLI,
-					hasStatusOK,
-					hasMetRequestLatencySLO,
-					requestLatency)
+			// 	results.Record(hasPassedSLI,
+			// 		hasStatusOK,
+			// 		hasMetRequestLatencySLO,
+			// 		requestLatency)
 
-			}
+			// }
 
-			results.PrintResults()
+			// results.PrintResults()
 
-			Expect(results.SuccessPercentage()).To(BeNumerically(">=", dataPlaneSLOPercentage))
+			// Expect(results.SuccessPercentage()).To(BeNumerically(">=", dataPlaneSLOPercentage))
 		})
 	})
 })
--- test/uptime/uptime_tests_suite_test.go
+++ test/uptime/uptime_tests_suite_test.go
@@ -64,9 +64,8 @@ var _ = BeforeSuite(func() {
 	Expect(err).NotTo(HaveOccurred(), "CONTROL_PLANE_SLO_DATA_PLANE_AVAILABILITY_PERCENTAGE malformed")
 
 	cfAppDomain := getEnvOrUseDefault("CF_APP_DOMAIN", "apps.ci-upgrade-cf.routing.lol")
-	fmt.Printf("CF APP DOMAIN IN SUITE IS %s!!!!!!!!!!!!!!!!!!!!!!!!!!!\n", cfAppDomain)
 
-	getEnvOrUseDefault("CONTROL_PLANE_APP_NAME", "upgrade-control-plane-sli")
+	controlPlaneAppName := getEnvOrUseDefault("CONTROL_PLANE_APP_NAME", "upgrade-control-plane-sli")
 
 	dataPlaneAppName := getEnvOrUseDefault("DATA_PLANE_APP_NAME", "upgrade-data-plane-sli")
 
