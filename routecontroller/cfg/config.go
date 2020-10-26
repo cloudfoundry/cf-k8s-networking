2e25c5856583626f6feab0a721f6a111fc726c50
Clay Kauzlaric <ckauzlaric@vmware.com>
2020-10-26 22:01:24 +0000

    feat: routecontroller INGRESS_SOLUTION config option
    * it can accept different ingress providers, right now either istio or
    contour.
    * contour won't do anything yet
    
    Co-authored-by: Mikael Manukyan <mmanukyan@vmware.com>

--- routecontroller/cfg/config.go
+++ routecontroller/cfg/config.go
@@ -7,9 +7,17 @@ import (
 	"time"
 )
 
+type IngressProvider string
+
+const (
+	Istio   = IngressProvider("istio")
+	Contour = IngressProvider("contour")
+)
+
 type Config struct {
-	ResyncInterval time.Duration
-	Istio          struct {
+	ResyncInterval  time.Duration
+	IngressProvider IngressProvider
+	Istio           struct {
 		// The Istio Gateway the route controller applies to
 		Gateway string
 	}
@@ -18,17 +26,32 @@ type Config struct {
 func Load() (*Config, error) {
 	c := &Config{}
 	var exists bool
-	c.Istio.Gateway, exists = os.LookupEnv("ISTIO_GATEWAY_NAME")
+	var ingress string
+
+	ingress, exists = os.LookupEnv("INGRESS_SOLUTION")
+	c.IngressProvider = IngressProvider(ingress)
 
 	if !exists {
-		return nil, errors.New("ISTIO_GATEWAY_NAME not configured")
+		return nil, errors.New("INGRESS_SOLUTION not configured")
+	}
+
+	switch c.IngressProvider {
+	case Istio:
+		err := loadIstioConfig(c)
+		if err != nil {
+			return nil, err
+		}
+	case Contour:
+		loadContour(c)
+	default:
+		return nil, errors.New(fmt.Sprintf("INGRESS_SOLUTION=%s not supported", c.IngressProvider))
 	}
 
 	var err error
-	resync_interval, exists := os.LookupEnv("RESYNC_INTERVAL")
+	resyncInterval, exists := os.LookupEnv("RESYNC_INTERVAL")
 
 	if exists {
-		c.ResyncInterval, err = time.ParseDuration(fmt.Sprintf("%ss", resync_interval))
+		c.ResyncInterval, err = time.ParseDuration(fmt.Sprintf("%ss", resyncInterval))
 		if err != nil {
 			return nil, errors.New("could not parse the RESYNC_INTERVAL duration")
 		}
@@ -37,4 +60,17 @@ func Load() (*Config, error) {
 	}
 
 	return c, nil
+}
+
+func loadContour(c *Config) {}
+
+func loadIstioConfig(c *Config) error {
+	var exists bool
+
+	c.Istio.Gateway, exists = os.LookupEnv("ISTIO_GATEWAY_NAME")
+	if !exists {
+		return errors.New("ISTIO_GATEWAY_NAME not configured")
+	}
+
+	return nil
 }
--- routecontroller/cfg/config_test.go
+++ routecontroller/cfg/config_test.go
@@ -12,43 +12,103 @@ import (
 var _ = Describe("Config", func() {
 	Describe("Load", func() {
 		BeforeEach(func() {
-			err := os.Setenv("ISTIO_GATEWAY_NAME", "some-gateway")
+			err := os.Setenv("INGRESS_SOLUTION", "istio")
 			Expect(err).NotTo(HaveOccurred())
-			err = os.Setenv("RESYNC_INTERVAL", "15")
+			err = os.Setenv("ISTIO_GATEWAY_NAME", "some-gateway")
 			Expect(err).NotTo(HaveOccurred())
 		})
-
-		It("loads the config", func() {
-			config, err := cfg.Load()
-			Expect(err).NotTo(HaveOccurred())
-
-			Expect(config.Istio.Gateway).To(Equal("some-gateway"))
-			Expect(config.ResyncInterval).To(Equal(15 * time.Second))
+		AfterEach(func() {
+			os.Clearenv()
 		})
 
-		Context("when the ISTIO_GATEWAY_NAME env var is not set", func() {
+		Context("when the RESYNC_INTERVAL env var is set", func() {
 			BeforeEach(func() {
-				err := os.Unsetenv("ISTIO_GATEWAY_NAME")
+				err := os.Setenv("RESYNC_INTERVAL", "15")
 				Expect(err).NotTo(HaveOccurred())
 			})
+			It("is configured correctly ", func() {
+				config, err := cfg.Load()
+				Expect(err).NotTo(HaveOccurred())
 
-			It("returns an error", func() {
-				_, err := cfg.Load()
-				Expect(err).To(MatchError("ISTIO_GATEWAY_NAME not configured"))
+				Expect(config.ResyncInterval).To(Equal(15 * time.Second))
 			})
-
 		})
+
 		Context("when the RESYNC_INTERVAL env var is not set", func() {
-			BeforeEach(func() {
-				err := os.Unsetenv("RESYNC_INTERVAL")
-				Expect(err).NotTo(HaveOccurred())
-			})
-
 			It("defaults to 30 seconds", func() {
 				config, err := cfg.Load()
 				Expect(err).NotTo(HaveOccurred())
 				Expect(config.ResyncInterval).To(Equal(30 * time.Second))
 			})
+		})
+
+		Context("when the INGRESS_SOLUTION env var is not set", func() {
+			BeforeEach(func() {
+				err := os.Unsetenv("INGRESS_SOLUTION")
+				Expect(err).NotTo(HaveOccurred())
+			})
+			It("returns an error", func() {
+				_, err := cfg.Load()
+				Expect(err).To(MatchError("INGRESS_SOLUTION not configured"))
+			})
+		})
+
+		Context("when the INGRESS_SOLUTION env var is set to an invalid provider", func() {
+			BeforeEach(func() {
+				err := os.Setenv("INGRESS_SOLUTION", "some-ingress-solution")
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("returns an error", func() {
+				_, err := cfg.Load()
+				Expect(err).To(HaveOccurred())
+				Expect(err).To(MatchError("INGRESS_SOLUTION=some-ingress-solution not supported"))
+			})
+		})
+
+		Context("When INGRESS_SOLUTION env var is set to Istio", func() {
+			BeforeEach(func() {
+				err := os.Setenv("ISTIO_GATEWAY_NAME", "some-gateway")
+				Expect(err).NotTo(HaveOccurred())
+				err = os.Setenv("INGRESS_SOLUTION", "istio")
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("is configured correctly ", func() {
+				config, err := cfg.Load()
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(config.Istio.Gateway).To(Equal("some-gateway"))
+				Expect(config.IngressProvider).To(Equal(cfg.IngressProvider("istio")))
+			})
+
+			Context("when the ISTIO_GATEWAY_NAME env var is not set", func() {
+				BeforeEach(func() {
+					err := os.Unsetenv("ISTIO_GATEWAY_NAME")
+					Expect(err).NotTo(HaveOccurred())
+				})
+
+				It("returns an error", func() {
+					_, err := cfg.Load()
+					Expect(err).To(MatchError("ISTIO_GATEWAY_NAME not configured"))
+				})
+
+			})
+		})
+
+		Context("When INGRESS_SOLUTION env var is set to Contour", func() {
+			BeforeEach(func() {
+				err := os.Setenv("INGRESS_SOLUTION", "contour")
+				Expect(err).NotTo(HaveOccurred())
+			})
+
+			It("is configured correctly", func() {
+				config, err := cfg.Load()
+				Expect(err).NotTo(HaveOccurred())
+
+				Expect(config.IngressProvider).To(Equal(cfg.IngressProvider("contour")))
+			})
+
 		})
 	})
 })
