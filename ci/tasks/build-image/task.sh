5fd3184c733a9605f981c5bec8e55f8190735f8d
Ashwin Krishna <krishnaas@vmware.com>
2021-06-03 16:18:37 -0600

    Replace build with kbld build steps
    [#177547432](https://www.pivotaltracker.com/story/show/177547432)

---
+++ ci/tasks/build-image/task.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -euo pipefail
+
+trap "pkill dockerd" EXIT
+
+start-docker &
+echo 'until docker info; do sleep 5; done' >/usr/local/bin/wait_for_docker
+chmod +x /usr/local/bin/wait_for_docker
+timeout 300 wait_for_docker
+
+<<<"$DOCKERHUB_PASSWORD" docker login --username "$DOCKERHUB_USERNAME" --password-stdin
+
+cf-k8s-networking/build/build.sh
+
+image_ref="$(yq -r '.overrides[] | select(.image | test("/cf-k8s-networking")).newImage' cf-k8s-networking/build/kbld.lock.yml)"
+sed -i'' -e "s| routecontroller:.*| routecontroller: \"$image_ref\"|" cf-k8s-networking/config/values/images.yml
+
+pushd cf-k8s-networking > /dev/null
+  git config user.name "${GIT_COMMIT_USERNAME}"
+  git config user.email "${GIT_COMMIT_EMAIL}"
+  git add config/values/images.yml
+
+  # dont make a commit if there aren't new images
+  if ! git diff --cached --exit-code; then
+    echo "committing!"
+    git commit -m "images.yml updated by CI"
+  else
+    echo "no changes to images, not bothering with a commit"
+  fi
+popd > /dev/null
+
+cp -R cf-k8s-networking/. updated-cf-k8s-networking/
